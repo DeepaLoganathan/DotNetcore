1305700e0009d375c73652e35dc3903688f7f2dc
Deepa Loganathan <deepaloganathan@syncfusion.com>
2018-08-07 17:17:44 +0530

    File upload support

--- MVCHome/sample/sample/Controllers/DefaultController.cs
+++ MVCHome/sample/sample/Controllers/DefaultController.cs
@@ -12,11 +12,16 @@ using System.Runtime.Serialization.Json;
 
 namespace sample.Controllers
 {
+
+    public class Version
+    {
+        public string sync_ver { get; set; }
+    }
     public class DefaultController : Controller
     {
         private string generatedUrl;
         private string _version;
-
+        private static string path;
 
         // GET: Default
         public ActionResult Index()
@@ -63,7 +68,7 @@ namespace sample.Controllers
         public ActionResult Index(ProjectConfig mbProjectConfig)
         {
 
-                ViewBag.dummy = "SUCCESS";
+            ViewBag.dummy = "SUCCESS";
             String currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
             DirectoryInfo currentDirectoryInfo = new DirectoryInfo(currentDirectory);
             String grandParentPath = currentDirectoryInfo.Parent.Parent.Parent.FullName;
@@ -74,62 +79,140 @@ namespace sample.Controllers
 
             foreach (string currentargument in arguments)
             {
-                Process p = new Process();
-                p.StartInfo.FileName = "cmd.exe";
-                p.StartInfo.Arguments = currentargument;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardInput = true;
-                p.StartInfo.RedirectStandardError = true;
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.WorkingDirectory = @"E:\";
-                p.StartInfo.CreateNoWindow = true;
-
-                p.Start();
-
-
-                if (currentargument == "dotnet run")
+                using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
                 {
-                    KillDotNetProcess();
-                }
+                    Process p = new Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.Arguments = currentargument;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.WorkingDirectory = @"E:\";
+                    p.StartInfo.CreateNoWindow = true;
 
-                p.StandardInput.WriteLine(@"cd " + lstrRelPath);
-                if (currentargument == "dotnet run")
-                    p.StandardInput.WriteLine(currentargument + " --framework " + mbProjectConfig.net_ver);
-                else
-                    p.StandardInput.WriteLine(currentargument);
-                p.StandardInput.Flush();
-                p.StandardInput.Close();
-                
-                var output = "";
-                if (currentargument == "dotnet run")
-                {
-
-                    p.BeginOutputReadLine();
-
-                    p.OutputDataReceived += (sender, args) =>
+                    p.Start();
+                    string output; int exitCode;
+                    var timeout = 30000;
+                    if (currentargument == "dotnet run")
                     {
-                        output += args.Data;
+                        KillDotNetProcess();
+                    }
 
-                    };
-
-                    //p.WaitForExit();
-                    Thread.Sleep(15000);
-
-                    consoleoutput = output;
-                    var index = consoleoutput.IndexOf("http");
-                    if (index > -1)
+                    p.StandardInput.WriteLine(@"cd " + lstrRelPath);
+                    if (currentargument != "dotnet run")
                     {
-                        int lIntindex = consoleoutput.IndexOf("http");
-                    int lIntLength = consoleoutput.IndexOf("Application") - lIntindex;
-                    string url = consoleoutput.Substring(lIntindex, lIntLength);
-                    generatedUrl = url + "/" + mbProjectConfig.controller + "/" + mbProjectConfig.view; ;
-                }
-                else
-                    generatedUrl = "Retry after sometime";
-            }
-            }
-            return Content(generatedUrl);
+                        p.StandardInput.WriteLine(currentargument);
+                        p.StandardInput.Flush();
+                        p.StandardInput.Close();
+                    }
 
+                    if (currentargument == "dotnet run")
+                    {
+                        p.StandardInput.WriteLine(currentargument + " --framework " + mbProjectConfig.net_ver);
+                        p.StandardInput.Flush();
+                        p.StandardInput.Close();
+                        var outputBuilder = new StringBuilder();
+                        var errorBuilder = new StringBuilder();
+                        //******************************
+
+                        try
+                        {
+                            p.BeginOutputReadLine();
+                            p.BeginErrorReadLine();
+                            p.OutputDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                outputWaitHandle.Set();
+                            }
+                            else
+                            {
+                                outputBuilder.AppendLine(e.Data);
+                            }
+                        };
+                            p.ErrorDataReceived += (sender, e) =>
+                            {
+                                if (e.Data == null)
+                                {
+                                    errorWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    outputBuilder.AppendLine(e.Data);
+                                }
+                            };
+
+                            p.Start();
+
+
+                            bool exited = p.WaitForExit(timeout);
+
+                            //exitCode = p.ExitCode;
+                            consoleoutput = outputBuilder.ToString();
+                            var index = consoleoutput.IndexOf("http");
+                           if (index > -1)
+                            {
+                                int lIntindex = consoleoutput.IndexOf("http");
+                                int lIntLength = consoleoutput.IndexOf("Application") - lIntindex;
+                                string url = consoleoutput.Substring(lIntindex, lIntLength);
+                                generatedUrl = url + "/" + mbProjectConfig.controller + "/" + mbProjectConfig.view; ;
+                            }
+                            else
+                            {
+                                generatedUrl = "Retry after sometime";
+                            }
+
+                        }
+                        finally
+                        {
+                            outputWaitHandle.WaitOne(0);
+                            errorWaitHandle.WaitOne(0);
+                        }
+                    }
+
+                    //******************************
+
+
+
+
+
+
+
+
+
+                    //var output = "";
+                    //if (currentargument == "dotnet run")
+                    //{
+
+                    //    p.BeginOutputReadLine();
+
+                    //    p.OutputDataReceived += (sender, args) =>
+                    //    {
+                    //        output += args.Data;
+
+                    //    };
+
+                    //    //p.WaitForExit();
+                    //    Thread.Sleep(15000);
+
+
+
+                    //    consoleoutput = output;
+                    //var index = consoleoutput.IndexOf("http");
+                    //if (index > -1)
+                    //{
+                    //    int lIntindex = consoleoutput.IndexOf("http");
+                    //    int lIntLength = consoleoutput.IndexOf("Application") - lIntindex;
+                    //    string url = consoleoutput.Substring(lIntindex, lIntLength);
+                    //    generatedUrl = url + "/" + mbProjectConfig.controller + "/" + mbProjectConfig.view; ;
+                    //}
+                    //else
+                    //    generatedUrl = "Retry after sometime";
+                }
+            }
+            return Content("Generated URL: " + generatedUrl);
 
         }
 
@@ -140,57 +223,32 @@ namespace sample.Controllers
 
 
         [AcceptVerbs("Post")]
-        public void Save(IEnumerable<HttpPostedFileBase> UploadDefault, string UploadDefault_data)
+        public void Save()
         {
+            HttpContext lHttpContext = System.Web.HttpContext.Current;
             try
             {
-                if (System.Web.HttpContext.Current.Request.Files.AllKeys.Length > 0)
+                if (lHttpContext.Request.Files.AllKeys.Length > 0)
                 {
-                    var httpPostedChunkFile = System.Web.HttpContext.Current.Request.Files["chunkFile"];
-                    if (httpPostedChunkFile != null)
-                    {
-                        using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(HttpContext.Request.Form[0])))
-                        {
-                            // Deserialization from JSON  
-                            DataContractJsonSerializer deserializer = new DataContractJsonSerializer(typeof(Version));
-                            Version bsObj2 = (Version)deserializer.ReadObject(ms);
-                            this._version = bsObj2.sync_ver;
-                        }
-                        string path = "//netcoreapp//Syncfusion_" + this._version + "//controllers";
-                        var saveFile = System.Web.HttpContext.Current.Server.MapPath("UploadedFiles");
-                        var SaveFilePath = Path.Combine(saveFile, httpPostedChunkFile.FileName + ".part");
-                        var chunkIndex = System.Web.HttpContext.Current.Request.Form["chunk-index"];
-                        if (chunkIndex == "0")
-                        {
-                            //httpPostedChunkFile.SaveAs(SaveFilePath);
-                        }
-                        else
-                        {
-                            //  MergeChunkFile(SaveFilePath, httpPostedChunkFile.InputStream);
-                            var totalChunk = System.Web.HttpContext.Current.Request.Form["total-chunk"];
-                            if (Convert.ToInt32(chunkIndex) == (Convert.ToInt32(totalChunk) - 1))
-                            {
-                                var savedFile = System.Web.HttpContext.Current.Server.MapPath("UploadedFiles");
-                                var originalFilePath = Path.Combine(savedFile, httpPostedChunkFile.FileName);
-                                System.IO.File.Move(SaveFilePath, originalFilePath);
-                            }
-                        }
-                        HttpResponse ChunkResponse = System.Web.HttpContext.Current.Response;
-                        ChunkResponse.Clear();
-                        ChunkResponse.ContentType = "application/json; charset=utf-8";
-                        ChunkResponse.StatusDescription = "File uploaded succesfully";
-                        ChunkResponse.End();
-                    }
-                    var httpPostedFile = System.Web.HttpContext.Current.Request.Files["UploadFiles"];
+                    var httpPostedFile = lHttpContext.Request.Files["controller_file"];
 
                     if (httpPostedFile != null)
                     {
-                        var fileSave = System.Web.HttpContext.Current.Server.MapPath("UploadedFiles");
+                        if (httpPostedFile.FileName.EndsWith("cs"))
+                        {
+                            path = "//netcoreapp//Syncfusion_" + lHttpContext.Request.Form[0].ToString() + "//controllers";
+                        }
+                        else if (httpPostedFile.FileName.EndsWith("cshtml"))
+                        {
+                            path = "//netcoreapp//Syncfusion_" + lHttpContext.Request.Form[0].ToString() + "//views//" + lHttpContext.Request.Form[1].ToString();
+                        }
+                        string targetFolder = path;
+                        var fileSave = lHttpContext.Server.MapPath(targetFolder);
                         var fileSavePath = Path.Combine(fileSave, httpPostedFile.FileName);
                         if (!System.IO.File.Exists(fileSavePath))
                         {
                             httpPostedFile.SaveAs(fileSavePath);
-                            HttpResponse Response = System.Web.HttpContext.Current.Response;
+                            HttpResponse Response = lHttpContext.Response;
                             Response.Clear();
                             Response.ContentType = "application/json; charset=utf-8";
                             Response.StatusDescription = "File uploaded succesfully";
@@ -198,10 +256,10 @@ namespace sample.Controllers
                         }
                         else
                         {
-                            HttpResponse Response = System.Web.HttpContext.Current.Response;
+                            HttpResponse Response = lHttpContext.Response;
                             Response.Clear();
-                            Response.Status = "204 File already exists";
-                            Response.StatusCode = 204;
+                            Response.Status = "400 File already exists";
+                            Response.StatusCode = 400;
                             Response.StatusDescription = "File already exists";
                             Response.End();
                         }
@@ -213,8 +271,8 @@ namespace sample.Controllers
                 HttpResponse Response = System.Web.HttpContext.Current.Response;
                 Response.Clear();
                 Response.ContentType = "application/json; charset=utf-8";
-                Response.StatusCode = 204;
-                Response.Status = "204 No Content";
+                Response.StatusCode = 400;
+                Response.Status = "400 No Content";
                 Response.StatusDescription = e.Message;
                 Response.End();
             }
